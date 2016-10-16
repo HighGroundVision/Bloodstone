a2b3eecf8c2605b5b64d6c6dc2347c05388f8b9a
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2016-10-16 15:05:40 -0400

    Import Prototype

---
+++ CompareImages/IconLoader.cs
@@ -0,0 +1,63 @@
+﻿using AForge.Imaging.Filters;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareImages
+{
+    public static class ImageLoader
+    {
+        public static IEnumerable<Bitmap> ExtractUltimates(Bitmap image)
+        {
+            // Needs to be based on Rez...!
+
+            //var defaultBounds = new Rectangle(225, 660, 95, 95); // 1600 * 920
+            var defaultBounds = new Rectangle(270, 760, 115, 115); // 1920 * 1080
+
+            for (int i = 0; i < 12; i++)
+            {
+                var left = defaultBounds.Left + (defaultBounds.Width * i);
+                var filter = new Crop(new Rectangle(left, defaultBounds.Top, defaultBounds.Width, defaultBounds.Height));
+                yield return filter.Apply(image);
+            }
+        }
+
+        public static Dictionary<int, Bitmap> LoadIcons(string path)
+        {
+            var icons = new Dictionary<int, Bitmap>();
+
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                var info = new FileInfo(file);
+
+                var id = int.Parse(info.Name.Replace(info.Extension, ""));
+                icons.Add(id, LoadIcon(file));
+            }
+
+            return icons;
+        }
+
+        public static Bitmap LoadIcon(string path)
+        {
+            // Needs to be based on Rez...!
+
+            //var filter = new ResizeBilinear(74, 67); // 1600 * 920
+            var filter = new ResizeBilinear(89, 81);  // 1920 * 1080
+
+            return filter.Apply(LoadBitmap(path));
+        }
+
+        public static Bitmap LoadBitmap(string path)
+        {
+            var source = Image.FromFile(path);
+            var orginal = new Bitmap(source);
+            return orginal.Clone(new Rectangle(0, 0, orginal.Width, orginal.Height), PixelFormat.Format24bppRgb);
+        }
+    }
+}
--- CompareImages/Program.cs
+++ CompareImages/Program.cs
@@ -3,13 +3,120 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dota2GSI;
+using Dota2GSI.Nodes;
+using System.Threading;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
 
 namespace CompareImages
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            using(var gsl = new GameStateListener(4000))
+            {
+                gsl.NewGameState += new NewGameStateHandler(OnNewGameState);
+
+                if (!gsl.Start())
+                {
+                    Console.WriteLine("GameStateListener could not start. Try running this program as Administrator.");
+                    Console.WriteLine("Exiting.");
+                    return;
+                }
+
+                Console.WriteLine("Listening for game integration calls...");
+                Console.WriteLine("Press any key to close.");
+                Console.ReadKey();
+
+                gsl.Stop();
+            }
+        }
+
+        static void OnNewGameState(GameState gs)
+        {
+            try
+            {
+                if (gs.Previously.Map.GameState == DOTA_GameState.DOTA_GAMERULES_STATE_WAIT_FOR_PLAYERS_TO_LOAD && gs.Map.GameState == DOTA_GameState.DOTA_GAMERULES_STATE_HERO_SELECTION)
+                {
+                    // Wait for ability draft screen to load.
+                    Thread.Sleep(3000); // Yes it takes 3 seconds to finish the load animation.
+
+                    // Take screen shot
+                    var template = ScreenCapture.CaptureApplication("dota2");
+
+                    // Extract ultimate images
+                    var icons = ImageLoader.ExtractUltimates(template).ToList();
+
+                    template.Save("tempalte.png", ImageFormat.Png);
+                    for (int i = 0; i < icons.Count; i++) icons[i].Save(string.Format("icon{0}.png", (i + 1)), ImageFormat.Png);
+
+                    // Process icons to find match
+                    var abilities = icons.AsParallel().Select(icon => MatchIcon(icon)).ToList();
+                    var keys = abilities.Distinct().ToList();
+
+                    // if full draft found then lunch web site
+                    if (keys.Count == 12)
+                    {
+                        var key = string.Join(",", keys);
+                        var url = string.Format("http://www.abilitydrafter.com/Draft?key={0}", key);
+                        Process.Start(url);
+                    }
+                    else
+                    {
+                        Process.Start("http://www.abilitydrafter.com/Draft");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Try again next time...
+            } 
+        }
+
+        private static int MatchIcon(Bitmap icon)
+        {
+            var abilities = ImageLoader.LoadIcons("icons");
+            var threshold = 1.0f;
+
+            var collection = new List<int>();
+            do
+            {
+                collection = abilities.Where(_ => icon.Contains(_.Value, threshold) == true).Select(_ => _.Key).ToList();
+                threshold -= 0.05f;
+            }
+            while (collection.Count == 0 && threshold > 0.5f);
+
+            return collection.FirstOrDefault();
+        }
+
+        private static List<int> MatchTemplate(Bitmap template)
+        {
+            var abilities = ImageLoader.LoadIcons("icons");
+            var keys = new List<int>();
+
+            foreach (var item in abilities)
+            {
+                var threshold = 1.0f;
+
+                do
+                {
+                    if (template.Contains(item.Value, threshold) == true)
+                    {
+                        keys.Add(item.Key);
+                        break;
+                    }
+
+                    threshold -= 0.05f;
+                }
+                while (threshold > 0.5f);
+            }
+
+            return keys;
         }
     }
+    
 }
