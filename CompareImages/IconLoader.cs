1c87fdb5d99bf642bb570c0a981021a85255c49a
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2016-10-17 20:31:42 -0400

    fixed icon for better matching update the loader to use the rex bounds for the icons size added class for the GSI setup and disabled for now

--- CompareImages/IconLoader.cs
+++ CompareImages/IconLoader.cs
@@ -14,22 +14,35 @@ namespace CompareImages
     {
         public static IEnumerable<Bitmap> ExtractUltimates(Bitmap image)
         {
-            // Needs to be based on Rez...!
+            var iconBounds = Rectangle.Empty;
+            var offset = 0;
+            
+            if(image.Width == 1920 && image.Height == 1080)
+            {
+                iconBounds = new Rectangle(284, 777, 89, 81);
+                offset = 114;
+            }
+            else  if (image.Width == 1600 && image.Height == 1050)
+            {
+                iconBounds = new Rectangle(183, 756, 86, 79);
+                offset = 110;
+            }
 
-            //var defaultBounds = new Rectangle(225, 660, 95, 95); // 1600 * 920
-            var defaultBounds = new Rectangle(284, 777, 89, 81); // 1920 * 1080
-
+            if (iconBounds.IsEmpty == true)
+                throw new ArgumentOutOfRangeException(nameof(iconBounds));
+            
             for (int i = 0; i < 12; i++)
             {
-                var left = defaultBounds.Left + (114 * i);
-                var filter = new Crop(new Rectangle(left, defaultBounds.Top, defaultBounds.Width, defaultBounds.Height));
+                var left = iconBounds.Left + (offset * i);
+                var filter = new Crop(new Rectangle(left, iconBounds.Top, iconBounds.Width, iconBounds.Height));
                 yield return filter.Apply(image);
             }
         }
 
-        public static Dictionary<int, Bitmap> LoadIcons(string path)
+        public static Dictionary<int, Bitmap> LoadIcons(string path, Size resizeTo)
         {
             var icons = new Dictionary<int, Bitmap>();
+            var filter = new ResizeBilinear(resizeTo.Width, resizeTo.Height);
 
             var files = Directory.GetFiles(path);
             foreach (var file in files)
@@ -37,20 +50,18 @@ namespace CompareImages
                 var info = new FileInfo(file);
 
                 var id = int.Parse(info.Name.Replace(info.Extension, ""));
-                icons.Add(id, LoadIcon(file));
+                var bitmap = LoadIcon(file, filter);
+
+                icons.Add(id, bitmap);
             }
 
             return icons;
         }
 
-        public static Bitmap LoadIcon(string path)
+        public static Bitmap LoadIcon(string path, ResizeBilinear filter)
         {
-            // Needs to be based on Rez...!
-
-            //var filter = new ResizeBilinear(74, 67); // 1600 * 920
-            var filter = new ResizeBilinear(89, 81);  // 1920 * 1080
-
-            return filter.Apply(LoadBitmap(path));
+            var bitmap = LoadBitmap(path);
+            return filter.Apply(bitmap);
         }
 
         public static Bitmap LoadBitmap(string path)
--- CompareImages/Program.cs
+++ CompareImages/Program.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Diagnostics;
-using Microsoft.Win32;
+
 
 namespace CompareImages
 {
@@ -18,14 +18,14 @@ namespace CompareImages
     {
         static void Main(string[] args)
         {
-            try
-            {
-                SetGameStateCFG();
-            }
-            catch(Exception ex)
-            {
-                Console.Error.WriteLine("Failed to set CFG: "  + ex.Message);
-            }
+            //try
+            //{
+            //    GamestateIntegration.CopyGSIFile();
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.Error.WriteLine("Failed to set CFG: "  + ex.Message);
+            //}
 
             using (var gsl = new GameStateListener(4000))
             {
@@ -40,7 +40,6 @@ namespace CompareImages
 
                 Console.WriteLine("Listening for game integration calls...");
                 Console.WriteLine("Press any key to close.");
-                Console.WriteLine("");
                 Console.ReadKey();
 
                 gsl.Stop();
@@ -53,6 +52,7 @@ namespace CompareImages
             {
                 if (gs.Previously.Map.GameState == DOTA_GameState.DOTA_GAMERULES_STATE_WAIT_FOR_PLAYERS_TO_LOAD && gs.Map.GameState == DOTA_GameState.DOTA_GAMERULES_STATE_HERO_SELECTION)
                 {
+                    Console.WriteLine("");
                     Console.WriteLine("Started Game, waiting for animation to finish.");
 
                     // Wait for ability draft screen to load.
@@ -62,21 +62,23 @@ namespace CompareImages
 
                     // Take screen shot
                     var template = ScreenCapture.CaptureApplication("dota2");
+
                     template.Save("tempalte.png", ImageFormat.Png);
 
                     Console.WriteLine("Captured screen. Extracting images for Ultimate's");
 
                     // Extract ultimate images
                     var icons = ImageLoader.ExtractUltimates(template).ToList();
+
                     for (int i = 0; i < icons.Count; i++) icons[i].Save(string.Format("icon{0}.png", (i + 1)), ImageFormat.Png);
 
                     Console.WriteLine("Extracted Ultimate's. Matching extractions to Ultimate's on file.");
 
                     // Process icons to find match
                     var abilities = icons.AsParallel().Select(icon => MatchIcon(icon)).ToList();
-                    var keys = abilities.Distinct().ToList();
-
+                    
                     // if full draft found then lunch web site
+                    var keys = abilities.Distinct().ToList();
                     if (keys.Count == 12)
                     {
                         Console.WriteLine("Draft found. Opening drafting page");
@@ -87,7 +89,7 @@ namespace CompareImages
                     }
                     else
                     {
-                        Process.Start("http://www.abilitydrafter.com/Draft");
+                        Console.WriteLine("Failed to draft. Check debug images.");
                     }
                 }
             }
@@ -100,7 +102,7 @@ namespace CompareImages
 
         private static int MatchIcon(Bitmap icon)
         {
-            var abilities = ImageLoader.LoadIcons("icons");
+            var abilities = ImageLoader.LoadIcons("icons", icon.Size);
             var threshold = 1.0f;
 
             var collection = new List<int>();
@@ -114,107 +116,8 @@ namespace CompareImages
             return collection.FirstOrDefault();
         }
 
-        private static List<int> MatchTemplate(Bitmap template)
-        {
-            var abilities = ImageLoader.LoadIcons("icons");
-            var keys = new List<int>();
 
-            foreach (var item in abilities)
-            {
-                var threshold = 1.0f;
-
-                do
-                {
-                    if (template.Contains(item.Value, threshold) == true)
-                    {
-                        keys.Add(item.Key);
-                        break;
-                    }
-
-                    threshold -= 0.05f;
-                }
-                while (threshold > 0.5f);
-            }
-
-            return keys;
-        }
-
-        static List<string> GetSteamLibFolders()
-        {
-            const string STEAMAPP_FOLDERPATH = "steamapps\\common";
-
-            List<string> folderPaths = new List<string>();
-
-            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Valve\\Steam");
-
-            if (key != null)
-            {
-                var steamInstallLocation = key.GetValue("SteamPath") as string;
-
-                
-                folderPaths.Add(System.IO.Path.Combine(steamInstallLocation, STEAMAPP_FOLDERPATH));
-
-                using (var sr = new System.IO.StreamReader(System.IO.Path.Combine(steamInstallLocation, "SteamApps", "libraryfolders.vdf")))
-                {
-                    bool start = false;
-                    while (!sr.EndOfStream)
-                    {
-                        var r = sr.ReadLine();
-                        if (!start && r == "{")
-                        {
-                            start = true;
-                        }
-                        else if (r == "}")
-                        {
-                            break;
-                        }
-                        else if (start)
-                        {
-                            var split = r.Split(new string[] { "\t\t" }, StringSplitOptions.RemoveEmptyEntries);
-                            int i = 0;
-                            if (int.TryParse(split[0].Trim().Trim('"'), out i))
-                            {
-                                var s = split[1].Trim().Trim('"').Replace("\\\\", "/");
-                                folderPaths.Add(System.IO.Path.Combine(s, STEAMAPP_FOLDERPATH));
-                            }
-                        }
-                    }
-
-                }
-            }
-
-            return folderPaths;
-        }
-
-        static void SetGameStateCFG()
-        {
-            var steamAppFolders = GetSteamLibFolders();
-
-            const string DOTAGAMEFOLDER = "dota 2 beta";
-            const string HGVCFG = "gamestate_integration_hgv.cfg";
-
-            foreach (var path in steamAppFolders)
-            {
-                var dotaPath = System.IO.Path.Combine(path, DOTAGAMEFOLDER);
-
-                var foundPath = System.IO.Directory.EnumerateDirectories(path).FirstOrDefault(x => x == dotaPath);
-                if(foundPath != null)
-                {
-                    var cfgPath = System.IO.Path.Combine(foundPath, "game\\dota\\cfg\\gamestate_integration");
-                    if (!System.IO.Directory.Exists(cfgPath))
-                        System.IO.Directory.CreateDirectory(cfgPath);
-
-                    var hgvCFGFilePath = System.IO.Path.Combine(cfgPath, HGVCFG);
-
-                    if (!System.IO.File.Exists(hgvCFGFilePath))
-                    {
-                        System.IO.File.Copy(HGVCFG, hgvCFGFilePath);
-                    }
-
-                    break;
-                }
-            }
-        }
+       
     }
     
 }
