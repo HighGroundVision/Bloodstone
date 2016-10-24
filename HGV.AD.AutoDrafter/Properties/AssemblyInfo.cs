1a93d3f1ac7999139d76c721614f332624a05eee
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2016-10-23 21:06:04 -0400

    Updated Icon

--- HGV.AD.AutoDrafter/Properties/AssemblyInfo.cs
+++ HGV.AD.AutoDrafter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HGV.AD.AutoDrafter")]
+[assembly: AssemblyTitle("AutoDrafter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HGV.AD.AutoDrafter")]
+[assembly: AssemblyCompany("Venatio Studios")]
+[assembly: AssemblyProduct("AutoDrafter")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
