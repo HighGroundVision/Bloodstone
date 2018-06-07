f9ed4d0bb88aed49bd1174bd131543e2b198ccec
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2018-06-07 08:29:44 -0400

    Remove old Added new

---
+++ src/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection;
+
+using Accord;
+using Accord.Imaging;
+using Accord.Imaging.Filters;
+using Accord.Math.Geometry;
+
+namespace HGV.Bloodstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bitmap = (Bitmap)Bitmap.FromFile("test1.png");
+
+            // lock image
+            BitmapData bitmapData = bitmap.LockBits(ImageLockMode.ReadWrite);
+
+            // step 1 - turn background to black
+            ColorFiltering colorFilter = new ColorFiltering();
+
+            colorFilter.Red = new IntRange(0, 64);
+            colorFilter.Green = new IntRange(0, 64);
+            colorFilter.Blue = new IntRange(0, 64);
+            colorFilter.FillOutsideRange = false;
+
+            colorFilter.ApplyInPlace(bitmapData);
+
+            // step 2 - locating objects
+            BlobCounter blobCounter = new BlobCounter();
+
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 5;
+            blobCounter.MinWidth = 5;
+
+            blobCounter.ProcessImage(bitmapData);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+
+            bitmap.UnlockBits(bitmapData);
+
+            // step 3 - check objects' type and highlight
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+
+            for (int i = 0; i < blobs.Length; i++)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+                List<IntPoint> corners;
+                if (shapeChecker.IsConvexPolygon(edgePoints, out corners))
+                {
+                    if(corners.Count == 4)
+                    {
+                        // step 4 - profit!
+                    }
+                }
+            }
+        }
+    }
+}
